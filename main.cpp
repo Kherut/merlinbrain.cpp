11067093d13356fc3b9bece31efbf0d95c06fdfa
Adam Pisula <adam.pisula@outlook.com>
2018-09-16 16:33:37 +0200

    GUI

--- main.cpp
+++ main.cpp
@@ -1,58 +1,89 @@
 #include <ncurses.h>
 #include <string>
+#include <cstring>
+#include <vector>
+
+using std::vector;
 
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
+void execute(const char *command, std::vector<char[256]> queue);
+void refreshQueue();
 
 int main(int argc, char *argv[])
-{	WINDOW *devices_win, *command_win, *queue_win;
+{	
+	WINDOW *devices_win, *command_win, *queue_win;
 	int x, y;
-	int ch;
+	char command[256];
+	std::vector<char[256]> queue;
 
 	initscr();
 	cbreak();
 
 	keypad(stdscr, TRUE);
 
-    /*printw(const_cast<char*>(std::to_string(LINES).c_str()));
-    printw("\n");
-    printw(const_cast<char*>(std::to_string(COLS).c_str()));*/
+	attron(A_BOLD);
+	mvprintw(0, COLS / 2 - 6, "Merlin Brain");
+	attroff(A_BOLD);
 
-	refresh();
+    refresh();
 
-    //y = 0;
-	//x = 0;
-	//devices_win = create_newwin(LINES, COLS / 3, y, x);
-    
-    y = COLS / 3;
-    x = 0;
-    command_win = create_newwin(4, COLS * 2 / 3, y, x);
+    y = 1;
+	x = 1;
+	devices_win = create_newwin(LINES - 1, COLS / 3 - 1, y, x);
+    mvwprintw(devices_win, 0, 1, "Devices");
 
-	while((ch = getch()) != KEY_F(1))
-	{	
-        
+    y = 1;
+    x = COLS / 3 + 1;
+    command_win = create_newwin(3, COLS * 2 / 3 - 1, y, x);
+	mvwprintw(command_win, 0, 1, "Command");
+	mvwprintw(command_win, 1, 1, " > ");
+
+	y = 4;
+    x = COLS / 3 + 1;
+    queue_win = create_newwin(LINES - 4, COLS * 2 / 3 - 1, y, x);
+	mvwprintw(queue_win, 0, 1, "Queue");
+
+	wrefresh(devices_win);
+	wrefresh(command_win);
+	wrefresh(queue_win);
+
+	while (true) {
+        mvwgetstr(command_win, 1, 4, command);
+		
+		//execute(command, queue);
+
+		for(int i = 4; i < COLS * 2 / 3 - 2; i++)
+			mvwprintw(command_win, 1, i, " ");
 	}
 		
 	endwin();
 	return 0;
 }
 
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{	WINDOW *local_win;
+WINDOW *create_newwin(int height, int width, int starty, int startx) {
+	WINDOW *local_win;
 
 	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
+	box(local_win, 0 , 0);
+	wrefresh(local_win);
 
 	return local_win;
 }
 
-void destroy_win(WINDOW *local_win)
-{	
+void destroy_win(WINDOW *local_win) {	
 	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
 	
 	wrefresh(local_win);
 	delwin(local_win);
 }
+ 
+/*void execute(const char command[256], std::vector<char[256]> queue) {
+	queue.push_back(command);
+}*/
+
+/*void refreshQueue(std::vector<char[256]> queue) {
+	for(std::vector<char[256]>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
+
+	}
+}*/
