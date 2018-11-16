2e79aed758c061fc512434ed732e4bdcf0a59898
Adam Pisula <adam.pisula@outlook.com>
2018-11-16 23:29:38 +0100

    Socket.io (command-line interface in advanced.html, event-driven communication between website and server)

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"os/exec"
 	"strings"
 	"time"
+	"bytes"
 
 	"github.com/robfig/cron"
+	"github.com/googollee/go-socket.io"
 )
 
 type Device struct {
@@ -31,8 +33,10 @@ func runCmd(cmd string) string {
 }
 
 func main() {
-	var devices []Device
 	development := true
+
+	var devices []Device
+	var socket socketio.Socket
 
 	//DEVELOPMENT
 	if !development {
@@ -54,9 +58,9 @@ func main() {
 	}
 
 	//DASHBOARD AT /dashboard
-	http.Handle("/dashboard/", http.StripPrefix("/dashboard/", http.FileServer(http.Dir("template"))))
+	http.Handle("/dashboard/", http.StripPrefix("/dashboard/", http.FileServer(http.Dir("www"))))
 
-	//CONTROL AT /control
+	//CONTROL AT /control (THERE'S ONLY devices/new IN THERE)
 	http.HandleFunc("/control/", func(w http.ResponseWriter, r *http.Request) {
 		command := strings.Join(strings.Split(r.URL.Path[1:], "/")[1:], "/")
 
@@ -68,67 +72,15 @@ func main() {
 			arg = strings.Split(command, "/")[1:]
 		}
 
-		//fmt.Print(category + " -> ")
-		//fmt.Println(arg)
-
-		if category == "info" {
-			switch arg[0] {
-			case "temperature":
-				if len(arg) > 1 {
-					if arg[1] == "all" {
-						//DEVELOPMENT
-						if !development {
-							w.Write([]byte(runCmd("cat ./data/temperature.data")))
-						}
-					}
-				} else {
-					//DEVELOPMENT
-					if !development {
-						w.Write([]byte(runCmd("cat /sys/devices/virtual/thermal/thermal_zone0/temp")))
-					}
-				}
-			case "uptime":
-				//DEVELOPMENT
-				if !development {
-					w.Write([]byte(strings.Split(runCmd("cat /proc/uptime"), ".")[0]))
-				}
-			}
-		} else if category == "led" {
-			var cmd string
-
-			switch arg[0] {
-			case "on":
-				//DEVELOPMENT
-				if !development {
-					cmd = "echo 1 > /sys/class/leds/red_led/brightness"
-				}
-
-			case "off":
-				//DEVELOPMENT
-				if !development {
-					cmd = "echo 0 > /sys/class/leds/red_led/brightness"
-				}
-			}
-
-			output := runCmd(cmd)
-
-			if len(output) > 0 {
-				w.Write([]byte("\n\nOutput: " + output))
-				fmt.Println(output)
-			}
-		} else if category == "devices" {
-			switch arg[0] {
-			case "new":
-				if len(arg) >= 4 {
+		if category == "devices" {
+			if len(arg) > 0 {
+				if (arg[0] == "new" && len(arg) >= 4) {
 					ip := arg[1]
 					name := arg[2]
 					role := arg[3]
+					connectedat := time.Now().Format("2006.01.02-15:04:05")
 
-					devices = append(devices, Device{Name: name, IP: ip, Role: role, Status: "UP", ConnectedAt: time.Now().Format("2006.01.02-15:04:05")})
-
-					fmt.Println(ip)
-					fmt.Println(name)
-					fmt.Println(role)
+					devices = append(devices, Device{Name: name, IP: ip, Role: role, Status: "UP", ConnectedAt: connectedat})
 
 					min := fmt.Sprintf("%d", 40000)
 					max := fmt.Sprintf("%d", 41000)
@@ -136,6 +88,8 @@ func main() {
 					port := runCmd("./get_port " + min + " " + max)
 
 					w.Write([]byte(port))
+
+					socket.Emit("message", command + "///" + "{\"name\": \"" + name + "\", \"ip\": \"" + ip + "\", \"role\": \"" + role + "\", \"status\": \"" + "UP" + "\", \"connectedat\": \"" + connectedat + "\"}")
 
 					var cmd string
 
@@ -151,34 +105,109 @@ func main() {
 
 					w.(http.Flusher).Flush()
 					runCmd(cmd)
-				} else {
-					w.Write([]byte("ERROR - Not enough arguments."))
-				}
-			case "all":
-				if len(arg) == 1 {
-					for _, element := range devices {
-						w.Write([]byte(element.Name + "\n"))
-						w.Write([]byte(element.IP + "\n"))
-						w.Write([]byte(element.Role + "\n"))
-						w.Write([]byte(element.Status + "\n"))
-						w.Write([]byte(element.ConnectedAt + "\n"))
-						w.Write([]byte("\n"))
-					}
-				} else if len(arg) >= 2 && arg[1] == "json" {
-					for _, element := range devices {
-						if element != (Device{"", "", "", "", ""}) {
-							w.Write([]byte("{ \"name\": \""))
-							w.Write([]byte(element.Name + "\", \"ip\": \""))
-							w.Write([]byte(element.IP + "\", \"role\": \""))
-							w.Write([]byte(element.Role + "\", \"status\": \""))
-							w.Write([]byte(element.Status + "\", \"connectedat\": \""))
-							w.Write([]byte(element.ConnectedAt + "\" }\n"))
-						}
-					}
 				}
 			}
 		}
 	})
+
+	//CONTROL ON /socket.io/
+	srvSio, _ := socketio.NewServer(nil)
+
+	srvSio.On("connection", func(sio socketio.Socket) {
+		socket = sio
+
+		socket.On("message", func(msg string) {
+			category := strings.Split(msg, "/")[0]
+
+			var arg []string
+
+			if len(strings.Split(msg, "/")) > 1 {
+				arg = strings.Split(msg, "/")[1:]
+			}
+
+			fmt.Print(category + " -> ")
+			fmt.Println(arg)
+
+			if category == "info" {
+				switch arg[0] {
+				case "temperature":
+					if len(arg) > 1 {
+						if arg[1] == "all" {
+							//DEVELOPMENT
+							if !development {
+								sio.Emit("message", runCmd("cat ./data/temperature.data"))
+							}
+						}
+					} else {
+						//DEVELOPMENT
+						if !development {
+							sio.Emit("message", runCmd("cat /sys/devices/virtual/thermal/thermal_zone0/temp"))
+						}
+					}
+				case "uptime":
+					//DEVELOPMENT
+					if !development {
+						sio.Emit("message", strings.Split(runCmd("cat /proc/uptime"), ".")[0])
+					}
+				}
+			} else if category == "led" {
+				var cmd string
+
+				switch arg[0] {
+				case "on":
+					//DEVELOPMENT
+					if !development {
+						cmd = "echo 1 > /sys/class/leds/red_led/brightness"
+					}
+
+				case "off":
+					//DEVELOPMENT
+					if !development {
+						cmd = "echo 0 > /sys/class/leds/red_led/brightness"
+					}
+				}
+
+				_ = runCmd(cmd)
+			} else if category == "devices" {
+				if len(arg) == 1 {
+					var text bytes.Buffer
+
+					text.WriteString(msg)
+					text.WriteString("///")
+
+					for _, element := range devices {
+						text.WriteString(element.Name + "\n")
+						text.WriteString(element.IP + "\n")
+						text.WriteString(element.Role + "\n")
+						text.WriteString(element.Status + "\n")
+						text.WriteString(element.ConnectedAt + "\n")
+					}
+
+					socket.Emit("message", text.String())
+				} else if len(arg) >= 2 && arg[1] == "json" {
+					var text bytes.Buffer
+
+					text.WriteString(msg)
+					text.WriteString("///")
+
+					for _, element := range devices {
+						if element != (Device{"", "", "", "", ""}) {
+							text.WriteString("{ \"name\": \"" + element.Name + "\", \"ip\": \"" + element.IP + "\", \"role\": \"" + element.Role + "\", \"status\": \"" + element.Status + "\", \"connectedat\": \"" + element.ConnectedAt + "\" }\n")
+						}
+					}
+
+					fmt.Println(text.String())
+					socket.Emit("message", text.String())
+				}
+			}
+		})
+	})
+
+	srvSio.On("error", func(so socketio.Socket, err error) {
+		fmt.Println("Error: ", err)
+	})
+
+	http.Handle("/socket.io/", srvSio)
 
 	http.HandleFunc("/", redirectDashboard)
 
